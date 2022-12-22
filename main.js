426cc61d2f7bf4d3851a9e63d95cabfdce16680d
Hungry_munk <abbaszadeharsam@gmail.com>
2022-12-23 10:31:14 +1100

    added linters and prettier (I think)

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+const name = "James";
+
+const person = { first: name };
+console.log(person);
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSxpQkFBaUI7QUFDakI7QUFDQTtBQUNBLGdDQUFnQyxNQUFNO0FBQ3RDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgbmFtZSA9IFwiSmFtZXNcIjtcclxuXHJcbmNvbnN0IHBlcnNvbiA9IHsgZmlyc3Q6IG5hbWUgfTtcclxuY29uc29sZS5sb2cocGVyc29uKTtcclxuY29uc3Qgc2F5SGVsbG9MaW50aW5nID0gKGZOYW1lKSA9PiB7XHJcbiAgY29uc29sZS5sb2coYEhlbGxvIGxpbnRpbmcsICR7Zk5hbWV9YCk7XHJcbn07XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
