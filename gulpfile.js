664a0dfaeade021e947cc2cc80d32769d90f12d5
Hungry_munk <abbaszadeharsam@gmail.com>
2022-12-22 20:01:06 +1100

    more changes to gulp file, css and scss file path in the both style and watcher functions

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const browserSync = require('browser-sync').create();
 
 function styles(done) {
   // Specify path of source SASS / SCSS file
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src('./scss/main.scss')
   // Compile it, and catch errors
   .pipe(sass().on('error',sass.logError))
   // Save the CSS output file in this path
@@ -21,7 +21,7 @@ function watch(done) {
     });
 
     // Watch for changes in the source SASS / SCSS file
-    gulp.watch('./scss/**/*.scss', styles);
+    gulp.watch('./scss/main.scss', styles);
     gulp.watch('./dist/*.css').on('change',browserSync.reload);
 
     done();
