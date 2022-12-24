e9b32a908e52214b00733a42c355317e6a1e7e82
Hungry_munk <abbaszadeharsam@gmail.com>
2022-12-25 08:35:01 +1100

    changing some prettier options

--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -1,115 +1,112 @@
 async function getLocationData(cityName, countryName) {
-	const geoLocationUrl = `https://api.api-ninjas.com/v1/geocoding?city=
+    const geoLocationUrl = `https://api.api-ninjas.com/v1/geocoding?city=
     	${cityName.toLowerCase()}&country=${countryName.toLowerCase()}`;
-	const options = {
-		method: 'GET',
-		mode: 'cors',
-		headers: { 'X-Api-Key': 'lYcUcXk1mj5TwE6n3NmVjA==bf4kIYw9tgEzFaZG' },
-		contentType: 'application/json'
-	};
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: { 'X-Api-Key': 'lYcUcXk1mj5TwE6n3NmVjA==bf4kIYw9tgEzFaZG' },
+        contentType: 'application/json'
+    };
 
-	try {
-		const locationData = await fetch(geoLocationUrl, options);
-		return [...(await locationData.json())][0];
-	} catch (err) {
-		if (err.response.status === 404) {
-			console.error('404, location wasnt found');
-		} else {
-			console.error(err.response.status);
-		}
-	}
+    try {
+        const locationData = await fetch(geoLocationUrl, options);
+        return [...(await locationData.json())][0];
+    } catch (err) {
+        if (err.response.status === 404) {
+            console.error('404, location wasnt found');
+        } else {
+            console.error(err.response.status);
+        }
+    }
 }
 
 async function getLocationWatherData(lat, lon) {
-	const apiKey = '4bd5549e876d085c663fab0828114f71';
-	const openWatherUrl = `https://api.openweathermap.org/data/2.5/weather
+    const apiKey = '4bd5549e876d085c663fab0828114f71';
+    const openWatherUrl = `https://api.openweathermap.org/data/2.5/weather
 		?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-	const options = {
-		method: 'GET',
-		mode: 'cors',
-		contentType: 'application/json'
-	};
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        contentType: 'application/json'
+    };
 
-	try {
-		const locationData = await fetch(openWatherUrl, options);
-		return await locationData.json();
-	} catch (err) {
-		if (err.response.status === 404) {
-			console.error('404, location wasnt found');
-		} else {
-			console.error(err.response.status);
-		}
-	}
+    try {
+        const locationData = await fetch(openWatherUrl, options);
+        return await locationData.json();
+    } catch (err) {
+        if (err.response.status === 404) {
+            console.error('404, location wasnt found');
+        } else {
+            console.error(err.response.status);
+        }
+    }
 }
 
 async function getLocationImageData(cityName, stateName) {
-	const apiKey = 'h6q1ZKjtqBKWTh3vLos8P11kdnF6x7T_Ap0G-eEoYEg';
-	const unsplashUrl = `https://api.unsplash.com/photos/random
+    const apiKey = 'h6q1ZKjtqBKWTh3vLos8P11kdnF6x7T_Ap0G-eEoYEg';
+    const unsplashUrl = `https://api.unsplash.com/photos/random
 		?query=${cityName} ${stateName}&orientation=landscape&client_id=${apiKey}`;
-	const options = {
-		method: 'GET',
-		mode: 'cors',
-		contentType: 'application/json'
-	};
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        contentType: 'application/json'
+    };
 
-	try {
-		const imageData = await fetch(unsplashUrl, options);
-		return await imageData.json();
-	} catch (err) {
-		console.error(err);
-	}
+    try {
+        const imageData = await fetch(unsplashUrl, options);
+        return await imageData.json();
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 export async function getCompleteApiData(cityName, country) {
-	const locationData = await getLocationData(cityName, country);
-	try {
-		// trying out cool deconstruciton syntax
-		const [weatherData, imageData] = await Promise.all([
-			getLocationWatherData(
-				locationData.latitude,
-				locationData.longitude
-			),
-			getLocationImageData(locationData.name, locationData.state)
-		]);
+    const locationData = await getLocationData(cityName, country);
+    try {
+        // trying out cool deconstruciton syntax
+        const [weatherData, imageData] = await Promise.all([
+            getLocationWatherData(locationData.latitude, locationData.longitude),
+            getLocationImageData(locationData.name, locationData.state)
+        ]);
 
-		return {
-			dataStatus: true,
-			completeLocationData: locationData,
-			completeWeatherData: weatherData,
-			completeimageData: imageData
-		};
-	} catch (err) {
-		console.error(new Error('location not found'));
-		return {
-			dataStatus: false
-		};
-	}
+        return {
+            dataStatus: true,
+            completeLocationData: locationData,
+            completeWeatherData: weatherData,
+            completeimageData: imageData
+        };
+    } catch (err) {
+        console.error(new Error('location not found'));
+        return {
+            dataStatus: false
+        };
+    }
 }
 
 export function filterApiData(completeApiData) {
-	const { completeWeatherData } = completeApiData;
-	const completeImageData = completeApiData.completeimageData;
-	const { completeLocationData } = completeApiData;
-	// returning data I actaully need
-	return {
-		locationData: {
-			city: completeLocationData.name,
-			state: completeLocationData.state
-		},
-		weatherData: {
-			temperatureDetails: completeWeatherData.main,
-			descriptionDetails: completeWeatherData.weather[0],
-			windDetails: completeWeatherData.wind
-		},
-		imageData: {
-			imageUrl: completeImageData.urls.full,
-			altDescription: completeImageData.alt_description,
-			creatorDetails: {
-				firstName: completeImageData.user.first_name,
-				lastName: completeImageData.user.last_name,
-				accountLink: completeImageData.user.links.html
-			}
-		}
-	};
+    const { completeWeatherData } = completeApiData;
+    const completeImageData = completeApiData.completeimageData;
+    const { completeLocationData } = completeApiData;
+    // returning data I actaully need
+    return {
+        locationData: {
+            city: completeLocationData.name,
+            state: completeLocationData.state
+        },
+        weatherData: {
+            temperatureDetails: completeWeatherData.main,
+            descriptionDetails: completeWeatherData.weather[0],
+            windDetails: completeWeatherData.wind
+        },
+        imageData: {
+            imageUrl: completeImageData.urls.full,
+            altDescription: completeImageData.alt_description,
+            creatorDetails: {
+                firstName: completeImageData.user.first_name,
+                lastName: completeImageData.user.last_name,
+                accountLink: completeImageData.user.links.html
+            }
+        }
+    };
 }
